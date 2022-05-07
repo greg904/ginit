dc84530683dbc6ec593f0a0eef56af287daefe87
Greg Depoire--Ferrer <greg@gregdf.com>
2022-05-08 01:44:55 +0200

    Fix bugs in init.c

--- init.c
+++ init.c
@@ -5,7 +5,8 @@
 #include <string.h>
 
 #include <fcntl.h>
-#include <libnetlink.h>
+#include <grp.h>
+// #include <libnetlink.h>
 #include <linux/netlink.h>
 #include <linux/rtnetlink.h>
 #include <net/if.h>
@@ -23,7 +24,7 @@
 
 static void mount_bubble()
 {
-	if (mount("/dev/nvme0n1p3", "/bubble", "ext4", 0, NULL) == -1)
+	if (mount("/dev/nvme0n1p2", "/bubble", "btrfs", 0, "subvol=/@bubble") == -1)
 		perror("mount(/bubble)");
 }
 
@@ -53,7 +54,7 @@ static void mount_special_fs()
 
 static void open_write_close(const char *file, const char *str)
 {
-	int fd = open(file, O_WRONLY);
+	int fd = open(file, O_WRONLY | O_CLOEXEC);
 	if (fd == -1) {
 		char buf[256];
 		snprintf(buf, sizeof(buf) / sizeof(buf[0]), "open(%s)", file);
@@ -90,7 +91,7 @@ static void set_sysctl_opts() {
 
 static void bring_if_up(const char *name)
 {
-	int fd = socket(PF_INET, SOCK_DGRAM, 0);
+	int fd = socket(PF_INET, SOCK_DGRAM | SOCK_CLOEXEC, 0);
 	if (fd == -1) {
 		perror("socket()");
 		return;
@@ -115,76 +116,108 @@ static void bring_if_up(const char *name)
 		perror("close");
 }
 
-static void set_eth0_addr()
+static bool nlmsg_add_attr(struct nlmsghdr *msg, int max_len, int type, const void *data, int data_len)
 {
-    struct {
-        struct nlmsghdr hdr;
-        struct ifaddrmsg ifa;
-        char buf[256];
-    } msg = {};
+	int offset = NLMSG_ALIGN(msg->nlmsg_len);
+	int rta_len = RTA_LENGTH(data_len);
+	if (offset + rta_len > max_len)
+		return false;
 
-    msg.hdr.nlmsg_len = NLMSG_LENGTH(sizeof(msg.ifa));
-    msg.hdr.nlmsg_flags = NLM_F_REQUEST | NLM_F_CREATE | NLM_F_EXCL;
-    msg.hdr.nlmsg_type = RTM_NEWADDR;
+	struct rtattr *rta = (struct rtattr *)((void *)msg + offset);
+	rta->rta_type = type;
+	rta->rta_len = rta_len;
 
-    msg.ifa.ifa_family = AF_INET;
-    msg.ifa.ifa_prefixlen = 24;
+	memcpy(RTA_DATA(rta), data, data_len);
+	msg->nlmsg_len = offset + rta_len;
+	return true;
+}
 
-    unsigned char addr[4] = { 192, 168, 1, 26 };
-    if (addattr_l(&msg.hdr, sizeof(msg), IFA_LOCAL, &addr, sizeof(addr)) == -1) {
-        perror("addattr_l()");
-        return;
-    }
-    if (addattr_l(&msg.hdr, sizeof(msg), IFA_ADDRESS, &addr, sizeof(addr)) == -1) {
-        perror("addattr_l()");
-        return;
-    }
+static void setup_eth0()
+{
+	// This is the message to set the IPv4 address.
+	struct {
+		struct nlmsghdr hdr;
+		struct ifaddrmsg ifa;
+		char buf[64];
+	} addr_msg = {};
+	addr_msg.hdr.nlmsg_len = NLMSG_LENGTH(sizeof(addr_msg.ifa));
+	addr_msg.hdr.nlmsg_flags = NLM_F_REQUEST | NLM_F_CREATE | NLM_F_EXCL;
+	addr_msg.hdr.nlmsg_seq = 0;
+	addr_msg.hdr.nlmsg_type = RTM_NEWADDR;
+	addr_msg.ifa.ifa_family = AF_INET;
+	addr_msg.ifa.ifa_prefixlen = 24;
+	const unsigned char addr[4] = { 192, 168, 1, 26 };
+	const unsigned char brd_addr[4] = { 255, 255, 255, 0 };
+	if (!nlmsg_add_attr(&addr_msg.hdr, sizeof(addr_msg), IFA_LOCAL, addr, sizeof(addr)) ||
+			!nlmsg_add_attr(&addr_msg.hdr, sizeof(addr_msg), IFA_ADDRESS, addr, sizeof(addr)) ||
+			!nlmsg_add_attr(&addr_msg.hdr, sizeof(addr_msg), IFA_BROADCAST, brd_addr, sizeof(brd_addr))) {
+		fputs("nlmsg_add_attr(): buffer is too small\n", stderr);
+		return;
+	}
 
-    unsigned char brd_addr[4] = { 255, 255, 255, 0 };
-    if (addattr_l(&msg.hdr, sizeof(msg), IFA_BROADCAST, &brd_addr, sizeof(brd_addr)) == -1) {
-        perror("addattr_l()");
-        return;
-    }
+	// This is the message to set the default route.
+	struct {
+		struct nlmsghdr hdr;
+		struct rtmsg rt;
+		char buf[64];
+	} rt_msg = {};
+	rt_msg.hdr.nlmsg_len = NLMSG_LENGTH(sizeof(rt_msg.rt));
+	rt_msg.hdr.nlmsg_flags = NLM_F_REQUEST | NLM_F_CREATE | NLM_F_EXCL;
+	rt_msg.hdr.nlmsg_seq = 1;
+	rt_msg.hdr.nlmsg_type = RTM_NEWROUTE;
+	rt_msg.rt.rtm_family = AF_INET;
+	rt_msg.rt.rtm_dst_len = 32;
+	rt_msg.rt.rtm_table = RT_TABLE_MAIN;
+	rt_msg.rt.rtm_protocol = RTPROT_BOOT;
+	rt_msg.rt.rtm_scope = RT_SCOPE_LINK;
+	rt_msg.rt.rtm_type = RTN_UNICAST;
+	const unsigned char gw_addr[4] = { 192, 168, 1, 254 };
+	if (!nlmsg_add_attr(&rt_msg.hdr, sizeof(rt_msg), RTA_GATEWAY, gw_addr, sizeof(gw_addr))) {
+		fputs("nlmsg_add_attr(): buffer is too small\n", stderr);
+		return;
+	}
     
-    struct rtnl_handle rth;
-    if (rtnl_open(&rth, 0) == -1) {
-        perror("rtnl_open()");
-        return;
-    }
-
-    if (rtnl_talk(&rth, &msg.hdr, 0, 0, NULL, NULL, NULL) == -1)
-        perror("rtnl_talk()");
+	int fd = socket(PF_NETLINK, SOCK_RAW | SOCK_CLOEXEC, NETLINK_ROUTE);
+	if (fd == -1) {
+		perror("socket(NETLINK_ROUTE)");
+		return;
+	}
+	if (send(fd, &addr_msg, addr_msg.hdr.nlmsg_len, 0) == -1 ||
+			send(fd, &rt_msg, rt_msg.hdr.nlmsg_len, 0) == -1)
+		perror("close(NETLINK_ROUTE)");
+	if (close(fd) == -1)
+		perror("close(NETLINK_ROUTE)");
 }
 
 static void start_udev()
 {
-	char *const envp[] = { "PATH=/bin:/sbin:/usr/bin:/usr/sbin", NULL };
+	char *const envp[] = { "PATH=/usr/bin", NULL };
 
-	char *const deamon_argv[] = { "/sbin/udevd", NULL };
+	char *const deamon_argv[] = { "/lib/systemd/systemd-udevd", NULL };
 	pid_t daemon_pid;
-	if (posix_spawn(&daemon_pid, "/sbin/udevd", NULL, NULL, deamon_argv, envp) != 0) {
-		perror("posix_spawn(/sbin/udevd)");
+	if (posix_spawn(&daemon_pid, "/lib/systemd/systemd-udevd", NULL, NULL, deamon_argv, envp) != 0) {
+		perror("posix_spawn(/lib/systemd/systemd-udevd)");
 		return;
 	}
 
-	char *const trigger_argv[] = { "/sbin/udevadm", "trigger", "--action=add", NULL };
+	char *const trigger_argv[] = { "/usr/bin/udevadm", "trigger", "--action=add", NULL };
 	pid_t trigger_pid;
-	if (posix_spawn(&trigger_pid, "/sbin/udevadm", NULL, NULL, trigger_argv, envp) != 0) {
-		perror("posix_spawn(/sbin/udevadm)");
+	if (posix_spawn(&trigger_pid, "/usr/bin/udevadm", NULL, NULL, trigger_argv, envp) != 0) {
+		perror("posix_spawn(/usr/bin/udevadm)");
 	} else {
 		int code;
 		if (waitpid(trigger_pid, &code, 0) == -1)
-			perror("waitpid(/sbin/udevadm)");
+			perror("waitpid(/usr/bin/udevadm)");
 	}
 
 	char *const settle_argv[] = { "/sbin/udevadm", "settle", NULL };
 	pid_t settle_pid;
-	if (posix_spawn(&settle_pid, "/sbin/udevadm", NULL, NULL, settle_argv, envp) != 0) {
-		perror("posix_spawn(/sbin/udevadm)");
+	if (posix_spawn(&settle_pid, "/usr/bin/udevadm", NULL, NULL, settle_argv, envp) != 0) {
+		perror("posix_spawn(/usr/bin/udevadm)");
 	} else {
 		int code;
 		if (waitpid(settle_pid, &code, 0) == -1)
-			perror("waitpid(/sbin/udevadm)");
+			perror("waitpid(/usr/bin/udevadm)");
 	}
 }
 
@@ -198,7 +231,7 @@ static pid_t start_sway()
 		if (setsid() == -1)
 			perror("setsid()");
 
-		int tty = open("/dev/tty0", O_RDWR | O_NOCTTY);
+		int tty = open("/dev/tty0", O_RDWR | O_CLOEXEC | O_NOCTTY);
 		if (tty == -1) {
 			perror("open(/dev/tty0)");
 		} else {
@@ -211,7 +244,7 @@ static pid_t start_sway()
 				perror("close()");
 		}
 
-		gid_t groups[] = { 10, 18, 23, 27, 1000 };
+		gid_t groups[] = { 24, 25, 27, 29, 30, 44, 46, 109, 113, 119, 1000 };
 		if (setgroups(sizeof(groups) / sizeof(groups[0]), groups) == -1) {
 			perror("setgroups()");
 			_exit(1);
@@ -225,8 +258,18 @@ static pid_t start_sway()
 			_exit(3);
 		}
 
+		if (chdir("/home/greg") == -1)
+			perror("chdir()");
+
 		char *const argv[] = { "/usr/bin/sway", NULL };
-		char *const envp[] = { "PATH=/bin:/sbin:/usr/bin:/usr/sbin", "XDG_RUNTIME_DIR=/run/me", "HOME=/home/me", NULL };
+		char *const envp[] = {
+			"HOME=/home/greg",
+			"PATH=/usr/bin",
+			"WLR_SESSION=direct",
+			"XDG_RUNTIME_DIR=/home/greg/xdg-runtime-dir",
+			"XDG_SEAT=seat-main",
+			NULL,
+		};
 		execvpe("/usr/bin/sway", argv, envp);
 		perror("execvpe(/usr/bin/sway)");
 		_exit(4);
@@ -241,10 +284,9 @@ int main()
 	set_backlight_brightness();
 	limit_battery_charge();
 	set_sysctl_opts();
-	bring_if_up("eth0");
-	set_eth0_addr();
 	bring_if_up("lo");
-	// ip route add default via 192.168.1.254 dev eth0
+	setup_eth0();
+	bring_if_up("eth0");
 
 	start_udev();
 	start_sway();
